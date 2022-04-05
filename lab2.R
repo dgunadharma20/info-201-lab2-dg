c5dc70aec329d828eaa3a91080be982c7a83ce31
dgunadharma20 <dgunadharma20@gmail.com>
2022-04-05 15:50:27 -0700

    added lab2 questions

---
+++ lab2.R
@@ -0,0 +1,18 @@
+library(stringr)
+
+# Write a function `money_conversion()` that takes in one numeric argument (`cents`) 
+# that converts any number of cents to the equivalent 
+# number of quarters, dimes, nickles, and pennies Your function should print the 
+# calculated number of quarters, dimes, nickles, and pennies on one line
+# hint: use the module operator
+
+# Create a vector `courses` that contains the names of 6 of your most favorite courses taken at UW
+
+## Write a function `replace_spaces` that will remove all spaces
+## from all elements in your vector of courses
+
+
+# Create a vector `numbers` that is the numbers 1 through 100
+
+# Create a vector called `larger_than_22` to store the numbers from your `numbers` 
+# vector that are larger than 22
