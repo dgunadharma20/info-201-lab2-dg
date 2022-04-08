481ed2588ed7c790bec5ce6894707fc4499b0d9c
dgunadharma20 <53589809+dgunadharma20@users.noreply.github.com>
2022-04-08 12:04:06 -0700

    Update lab2.R
    adding list problems

--- lab2.R
+++ lab2.R
@@ -6,12 +6,21 @@ library(stringr)
 # calculated number of quarters, dimes, nickles, and pennies on one line
 # hint: use the module operator
 
-# Create a vector `courses` that contains the names of 6 of your most favorite courses taken at UW
+# Create a vector `courses` that contains the names of 6 of your most favorite courses taken at UW in the format of "INFO 201"
 
 ## Write a function `replace_spaces` that will remove all spaces
-## from all elements in your vector of courses
+## from all elements in your vector of courses with a hyphen "-"
 
 
 # Create a vector `numbers` that is the numbers 1 through 100
 
 # Multiply all elements greater than 22 of the `numbers` vector with 2
+
+# Create a list `student` that has four tagged elements, `first_name`, `last_name`, `net_id` and `eligble_registration` where, 
+# `first_name` is a character, 
+# `last_name` is a character,
+# `net_id` is a numeric,
+# `eligble_registration` is a logical 
+# give each element a value based off of yourselves
+
+# Now, create a list without any tagged elements but give in the same values you gave for the previous problem
